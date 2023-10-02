641a9a9e4df0d5f1d24e48f3596919c36664d47d
fsam <sam.flaujat@ynov.com>
2023-10-02 16:06:17 +0200

    alldisplay

--- src/gameEngine/Display_HangMan.go
+++ src/gameEngine/Display_HangMan.go
@@ -16,11 +16,11 @@ func (g *Game) Display() {
 			g.stage = 1
 			g.Display()
 		}
-	case 1: //poteau
+	case 1: //sol
 		for i := 0; i < 9; i++ {
 			g.Tableau[g.Ligne-1][i] = "-"
 		}
-		
+
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
@@ -32,29 +32,149 @@ func (g *Game) Display() {
 			g.stage = 2
 			g.Display()
 		}
-	case 2: //barre du haut
+	case 2: //poteau
 		for i := 0; i < 7; i++ {
 			g.Tableau[i][4] = "|"
 		}
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		var text string
+		fmt.Scanln(&text)
 
-	case 3: //soutien
+		if text == "3" {
+			g.stage = 3
+			g.Display()
+		}
 
-	case 4: //corde
+	case 3: //barre du haut
+		for i := 4; i < 12; i++ {
+			g.Tableau[0][i] = "-"
+		}
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
 
-	case 5: //tete
+		if text == "4" {
+			g.stage = 4
+			g.Display()
+		}
 
-	case 6: //corps
+	case 4: //soutien
+		g.Tableau[1][6] = "/"
+		g.Tableau[2][5] = "/"
 
-	case 7: // bras gauche
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "5" {
+			g.stage = 5
+			g.Display()
+		}
+
+	case 5: //corde
+		g.Tableau[1][11] = "|"
+
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "6" {
+			g.stage = 6
+			g.Display()
+		}
+
+	case 6: //tete
+		g.Tableau[2][11] = "O"
+
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "7" {
+			g.stage = 7
+			g.Display()
+		}
+
+	case 7: // corps
+		g.Tableau[3][11] = "|"
+		g.Tableau[4][11] = "|"
+
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "8" {
+			g.stage = 8
+			g.Display()
+		}
 
 	case 8: // bras droit
+		g.Tableau[3][12] = `\`
 
-	case 9: // jambe gauche
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
 
-	case 10: // jambe droite
+		if text == "9" {
+			g.stage = 9
+			g.Display()
+		}
+
+	case 9: // bras gauche
+		g.Tableau[3][10] = "/"
+
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "10" {
+			g.stage = 10
+			g.Display()
+		}
+
+	case 10: // jambe gauche
+		g.Tableau[5][10] = "/"
+
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "11" {
+			g.stage = 11
+			g.Display()
+		}
+
+	case 11: // jambe droite
+		g.Tableau[5][12] = `\`
+
+		for i := 0; i < len(g.Tableau); i++ {
+			fmt.Println(g.Tableau[i])
+		}
+		var text string
+		fmt.Scanln(&text)
+
+		if text == "12" {
+			g.stage = 12
+			g.Display()
+		}
 
 	}
 
