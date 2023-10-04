6efe4a609288a9981dde2dcc662ff302d8879d4a
Lucas <lucas.savy@ynov.com>
2023-10-04 10:05:59 +0200

    game over

--- src/gameEngine/Display_HangMan.go
+++ src/gameEngine/Display_HangMan.go
@@ -4,12 +4,11 @@ import "fmt"
 
 func (g *Game) Display() {
 	switch g.stage {
-	case 0: //sol
-
+	case 0: //vide
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
-
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -19,7 +18,7 @@ func (g *Game) Display() {
 		}
 
 	case 1: //sol
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		for i := 0; i < 9; i++ {
 			g.Tableau[g.Ligne-1][i] = "-"
 		}
@@ -27,7 +26,7 @@ func (g *Game) Display() {
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
-
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -37,13 +36,14 @@ func (g *Game) Display() {
 		}
 
 	case 2: //poteau
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		for i := 0; i < 7; i++ {
 			g.Tableau[i][4] = "|"
 		}
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -53,13 +53,14 @@ func (g *Game) Display() {
 		}
 
 	case 3: //barre du haut
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		for i := 4; i < 12; i++ {
 			g.Tableau[0][i] = "-"
 		}
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -69,13 +70,14 @@ func (g *Game) Display() {
 		}
 
 	case 4: //soutien
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[1][6] = "/"
 		g.Tableau[2][5] = "/"
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -85,12 +87,13 @@ func (g *Game) Display() {
 		}
 
 	case 5: //corde
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[1][11] = "|"
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -100,12 +103,13 @@ func (g *Game) Display() {
 		}
 
 	case 6: //tete
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[2][11] = "O"
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -115,13 +119,14 @@ func (g *Game) Display() {
 		}
 
 	case 7: // corps
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[3][11] = "|"
 		g.Tableau[4][11] = "|"
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -131,12 +136,13 @@ func (g *Game) Display() {
 		}
 
 	case 8: // bras droit
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[3][12] = `\`
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -146,12 +152,13 @@ func (g *Game) Display() {
 		}
 
 	case 9: // bras gauche
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[3][10] = "/"
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -161,12 +168,13 @@ func (g *Game) Display() {
 		}
 
 	case 10: // jambe gauche
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[5][10] = "/"
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -176,12 +184,13 @@ func (g *Game) Display() {
 		}
 
 	case 11: // jambe droite
-
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 		g.Tableau[5][12] = `\`
 
 		for i := 0; i < len(g.Tableau); i++ {
 			fmt.Println(g.Tableau[i])
 		}
+		fmt.Print("\n\n")
 		var text string
 		fmt.Scanln(&text)
 
@@ -189,5 +198,16 @@ func (g *Game) Display() {
 			g.stage = 12
 			g.Display()
 		}
+	
+	case 12: // jambe droite
+		fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+		g.Tableau[5][12] = `\`
+		fmt.Println("  ________                        ________                     ")
+		fmt.Println(" /  _____/_____    _____   ____  ",`\`+"_____ ",`\`,"___  __ ___________ ")
+		fmt.Println("/  ",`\`," ___"+`\`+"__",`\`," /    ",`\`+"_/ __",`\`,"   /  |  ",`\`," ",`\`+"/ // __",`\`+"_  __",`\`)
+		fmt.Println(`\`,"  ",`\`+"_"+`\ `,`\`,"/ __ ",`\`,"|  Y Y  ",`\`,"  ___/  /    |    ",`\`,"/",`\`,"  ___/|  | ",`\`,"/")
+		fmt.Println(" ",`\`,"______  (____  /__|_|  /",`\`,"___  > ",`\`,"_______  /",`\`,"_/  ",`\`,"___  >__|   ")
+		fmt.Println(        \/     \/      \/     \/          \/          \/       )
+		fmt.Println("\n")
 	}
 }
